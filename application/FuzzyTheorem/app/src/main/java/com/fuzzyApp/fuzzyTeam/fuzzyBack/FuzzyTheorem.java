3b934c8516fb5fd7d6cb5830f077e4a8799ccf09
Austin Fell <austinrf2918@yahoo.com>
2017-04-15 13:49:37 -0400

    Refactored for easier team management.

---
+++ application/FuzzyTheorem/app/src/main/java/com/fuzzyApp/fuzzyTeam/fuzzyBack/FuzzyTheorem.java
@@ -0,0 +1,9 @@
+package com.fuzzyApp.fuzzyTeam.fuzzyBack;
+
+public class FuzzyTheorem {
+    public FuzzyTheorem(String databaseUri) {
+    }
+
+    public void clearDatabase() {
+    }
+}
--- application/FuzzyTheorem/app/src/main/java/com/fuzzyApp/fuzzyTeam/fuzzyFront/MainActivity.java
+++ application/FuzzyTheorem/app/src/main/java/com/fuzzyApp/fuzzyTeam/fuzzyFront/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.dominic.fuzzytheorem;
+package com.fuzzyApp.fuzzyTeam.fuzzyFront;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
