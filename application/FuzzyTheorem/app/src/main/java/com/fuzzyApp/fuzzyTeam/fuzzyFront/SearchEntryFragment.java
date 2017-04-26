e4efe4ee34c6331f43569102b591c3012dee4363
Austin Fell <austinrf2918@yahoo.com>
2017-04-26 00:17:23 -0400

    More stuff, refactoring

--- application/FuzzyTheorem/app/src/main/java/com/fuzzyApp/fuzzyTeam/fuzzyFront/SearchEntryFragment.java
+++ application/FuzzyTheorem/app/src/main/java/com/fuzzyApp/fuzzyTeam/fuzzyFront/SearchEntryFragment.java
@@ -1,6 +1,7 @@
 package com.fuzzyApp.fuzzyTeam.fuzzyFront;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,9 +16,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.fuzzyApp.fuzzyTeam.fuzzyBack.FuzzySearcher;
 import com.fuzzyApp.fuzzyTeam.fuzzyBack.fuzzyEntry.FuzzyEntry;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -36,12 +39,13 @@ public class SearchEntryFragment extends Fragment {
     private TextView entryTitleInput;
     private TextView newTagInput;
     private Spinner typeSelector;
-    private Button addTag;
+    private Button addTagButton;
 
     // Elements for binding to FuzzySearcher.
     private String currentType;
     private List<String> tagList;
     private ArrayList<FuzzyEntry> resultList;
+    private FuzzySearcher search;
 
     private ArrayAdapter<String> tagListAdapter;
 
@@ -62,6 +66,7 @@ public class SearchEntryFragment extends Fragment {
         dirty = false;
         tagsEmpty = true;
         currentType = "Lemma";
+        search = new FuzzySearcher();
 
         tagListView = (ListView) getView().findViewById(R.id.tag_list);
         resultListView = (ListView) getView().findViewById(R.id.result_list);
@@ -71,15 +76,34 @@ public class SearchEntryFragment extends Fragment {
 
         typeSelector = (Spinner) getView().findViewById(R.id.type_selector);
 
-        addTag = (Button) getView().findViewById(R.id.add_tag);
+        addTagButton = (Button) getView().findViewById(R.id.add_tag);
 
         tagList = new LinkedList<>();
         resultList = new ArrayList<>();
 
         tagListAdapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_list_item_1, tagList);
         tagListView.setAdapter(tagListAdapter);
+        tagListView.setOnItemClickListener(removeTag());
 
-        tagListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        addTagButton.setOnClickListener(addTag());
+    }
+
+    @NonNull
+    private View.OnClickListener addTag() {
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    addNewTag();
+                } catch (IllegalStateException e) {
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+    }
+
+    @NonNull
+    private AdapterView.OnItemClickListener removeTag() {
+        return new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = tagList.get(position);
@@ -89,31 +113,13 @@ public class SearchEntryFragment extends Fragment {
                     Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
-        });
-
-        addTag.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    addNewTag();
-                } catch (IllegalStateException e) {
-                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        };
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-    }
-
-    public void refreshCurrentTags() {
-        String newType = typeSelector.getSelectedItem().toString();
-
-        if (!newType.equals(currentType)) {
-            dirty = true;
-        }
     }
 
     public void addNewTag() throws IllegalStateException {
@@ -140,6 +146,16 @@ public class SearchEntryFragment extends Fragment {
             tagListAdapter.notifyDataSetChanged();
         } else {
             throw new IllegalStateException("Tag passed to removeTag does not exist.");
+        }
+    }
+
+    public void performSearch() {
+        HashSet<FuzzyEntry> nameMatches = search.filterByName(entryTitleInput.getText().toString());
+        HashSet<FuzzyEntry> typeMatches = search.filterByCategory(typeSelector.getSelectedItem().toString());
+
+        if (tagList.isEmpty()) {
+            HashSet<FuzzyEntry> tagMatches = search.filterByTags((ArrayList<String>) tagList);
+        } else {
         }
     }
 }
